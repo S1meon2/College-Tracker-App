36c6d21ae17628c703010eb7ba0b983c109fc341
ToTime <simeonherrod2@gmail.com>
2025-10-01 21:28:24 -0500

    main.py ~ Blackboard update
    The blackboard assignments can now be webscraped! This is by far the most userful (because most students use black board) and eaisiest ( because all assignments for every course is in one place ) update. * Keep in mind that the code may need to be tweaked becuase i've only tried it out on my assignments. *

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import csv
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import time
-import getpass
-
 while (True) :
     import csv
     from selenium import webdriver
@@ -13,7 +6,7 @@ while (True) :
     import time
     import getpass
 
-    print("What website do you need to get your homework from?  Options: Cengage, ZyBooks --> ")
+    print("What website do you need to get your homework from?  Options: Cengage, zyBooks, Blackboard --> ")
     web = input()
 
     if web == "Cengage":
@@ -29,14 +22,14 @@ while (True) :
         time.sleep(3)
         #   Input Username
         username = page_to_scrape.find_element(By.ID, "idp-discovery-username")
-        username.send_keys("/'YourUsername'/")
+        username.send_keys("/*username*/")
         #   Press Next
         page_to_scrape.find_element(By.ID, "idp-discovery-submit").click()
         #wait to load
         time.sleep(3)
         #   Input Username
         password = page_to_scrape.find_element(By.ID, "okta-signin-password")
-        my_pass = "/'YourPassword'/"
+        my_pass = "/*password*/"
         password.send_keys(my_pass)
         #   Sign in button
         page_to_scrape.find_element(By.ID, "okta-signin-submit").click()
@@ -68,7 +61,7 @@ while (True) :
         #--print(page_to_scrape.page_source)--
         #-------------^
 
-    if web == "ZyBooks":
+    if web == "zyBooks":
         #   Open Chrome
         page_to_scrape = webdriver.Chrome()
 
@@ -77,10 +70,10 @@ while (True) :
 
         #   Input Username
         username = page_to_scrape.find_element(By.CSS_SELECTOR, "input[type='email']")
-        username.send_keys("/'YourEmail'/")
+        username.send_keys("/*email*/")
         #   Input Password
         password = page_to_scrape.find_element(By.CSS_SELECTOR, "input[type='password']")
-        my_pass = "/'YourPassword'/"
+        my_pass = "/*password*/"
         password.send_keys(my_pass)
         #   Sign in button
         page_to_scrape.find_element(By.CLASS_NAME, "title").click()
@@ -104,7 +97,7 @@ while (True) :
             print(name.text + " - " + time.text)
         print()
 
-        
+
         #   End the webscraping
         page_to_scrape.quit()
 
@@ -114,6 +107,52 @@ while (True) :
         #--print(page_to_scrape.page_source)--
         # -------------^
 
-# If you want to check all the html use this:
-#--print(page_to_scrape.page_source)--
-#-------------^
+    if web == "Blackboard":
+        #   Open Chrome
+        page_to_scrape = webdriver.Chrome()
+
+        #   Open Website
+        page_to_scrape.get("https://ualearn.blackboard.com/")
+
+        time.sleep(5)
+
+        page_to_scrape.find_element(By.CLASS_NAME, "button-1").click()
+        #   Input Username
+        username = page_to_scrape.find_element(By.ID, "user_id")
+        username.send_keys("/*username*/")
+        #   Input Password
+        password = page_to_scrape.find_element(By.ID, "password")
+        my_pass = "/*password*/"
+        password.send_keys(my_pass)
+        #   Sign in button
+        page_to_scrape.find_element(By.ID, "entry-login").click()
+        # wait to load
+        time.sleep(8)
+        #   The Page we want
+        page_to_scrape.get("https://ualearn.blackboard.com/ultra/calendar")
+        page_to_scrape.find_element(By.ID, "bb-calendar1-deadline").click()
+        # wait to load
+        time.sleep(2)
+
+        # --------------------------------------------------------------------------------------
+
+        #   Pull the name of each assignment and thier corresponding times/dates
+        names = page_to_scrape.find_elements(By.CSS_SELECTOR,
+                                             "a[ng-click='viewDueDateItem(dueDateItem)']")
+        times = page_to_scrape.find_elements(By.CSS_SELECTOR,
+                                             "div[class='content']")
+
+        #   Each is printed
+        print()
+        for name, time in zip(names, times):
+            print(name.text + " - " + time.text)
+        print()
+
+        #   End the webscraping
+        page_to_scrape.quit()
+
+        # ----------------------------------------------------------------------------------------
+
+        # If you want to check all the html use this:
+        # --print(page_to_scrape.page_source)--
+        # -------------^
