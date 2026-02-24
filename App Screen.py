0d8f24f17dd9c96905e9faf326f96116c9022790
tommorowtime <simeonherrod2@gmail.com>
2026-02-23 22:21:06 -0600

    Rough version of the Front page. Nothing is funtional here yet. Sign In, Add Class, and Settings buttons have been added.

--- App Screen.py
+++ App Screen.py
@@ -1,13 +1,29 @@
-from kivy.app import App
-from kivy.uix.label import Label
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
-# MyApp(App)
-class MyApp(App):
+# First Page Visuals
+First_Page = '''
+MDScreen:
+    MDRaisedButton:
+        text: "Sign In"
+        pos_hint: {"top": 0.98, "right": 0.11}
+        
+    MDRaisedButton:
+        text: "Settings"
+        pos_hint: {"top": 0.98, "right": 0.98}
+        
+    MDRaisedButton:
+        text: "Add Class"
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        size_hint: (0.4, 0.1)
+    
+'''
+
+#Load first page
+class UBubbleApp(MDApp):
     def build(self):
-        return Label(text="Welcome to U-Bubble!")
-
-
+        return Builder.load_string(First_Page)
 
 # Run
-if __name__ == '__main__':
-    MyApp().run()
+if __name__ == "__main__":
+    UBubbleApp().run()
