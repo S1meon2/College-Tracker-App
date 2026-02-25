a77364eef4e4a4f0ba69a72d151c87577f09a8fb
tommorowtime <simeonherrod2@gmail.com>
2026-02-25 01:39:07 -0600

    Sign In, Add Class, and Settings buttons now take you to their respective pages. Apart from this nothing else functions yet.

--- App Screen.py
+++ App Screen.py
@@ -1,29 +1,97 @@
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
-# First Page Visuals
-First_Page = '''
-MDScreen:
+
+# Define the screens in KV
+AppUI = '''
+ScreenManager:
+    MainScreen:
+    SettingsScreen:
+    ClassAddScreen:
+    SignInScreen:
+<MainScreen>:
+    name: "main"
+
     MDRaisedButton:
         text: "Sign In"
         pos_hint: {"top": 0.98, "right": 0.11}
-        
+        on_release: root.manager.current = "sign in"
+
     MDRaisedButton:
         text: "Settings"
         pos_hint: {"top": 0.98, "right": 0.98}
-        
+        # This is the magic line that switches screens
+        on_release: root.manager.current = "settings"
+
     MDRaisedButton:
         text: "Add Class"
         pos_hint: {"center_x": 0.5, "center_y": 0.5}
-        size_hint: (0.4, 0.1)
+        on_release: root.manager.current = "add a class"
+
+<SettingsScreen>:
+    name: "settings"
+
+    MDLabel:
+        text: "Settings Page"
+        halign: "center"
+
+    MDRaisedButton:
+        text: "Back to Home"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        on_release: root.manager.current = "main"
+        
+<ClassAddScreen>:
+    name: "add a class"
     
+    MDLabel:
+        text: "+ ADD a class!"
+        halign: "center"
+        
+    MDRaisedButton:
+        text: "Back to Home"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        on_release: root.manager.current = "main"
+        
+<SignInScreen>:
+    name: "sign in"
+    
+    MDLabel:
+        text: "create username: "
+        halign: "left"
+        valign: "top"
+        
+    MDLabel:
+        text: "create pin number:"
+        halign: "right"
+        valign: "top"
+        
+    MDRaisedButton:
+        text: "Back to Home"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        on_release: root.manager.current = "main"
 '''
 
-#Load first page
+
+# We need these empty classes so Python recognizes the KV tags
+class MainScreen(Screen):
+    pass
+
+
+class SettingsScreen(Screen):
+    pass
+
+
+class ClassAddScreen(Screen):
+    pass
+
+class SignInScreen(Screen):
+    pass
+
 class UBubbleApp(MDApp):
     def build(self):
-        return Builder.load_string(First_Page)
+        return Builder.load_string(AppUI)
 
-# Run
+
 if __name__ == "__main__":
     UBubbleApp().run()
