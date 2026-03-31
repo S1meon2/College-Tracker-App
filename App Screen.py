d0858b1263c1e7b61f2f07f6bbc3e33288f2ee9f
tommorowtime <simeonherrod2@gmail.com>
2026-03-30 19:08:11 -0500

    added "Add account" page for zybooks. The program saves the info but does not do anything with it yet.

--- App Screen.py
+++ App Screen.py
@@ -3,17 +3,25 @@ from kivy.uix.screenmanager import Screen
 
 # We need these empty classes so Python recognizes the KV tags
 class MainScreen(Screen): pass
-class SettingsScreen(Screen): pass
+class SettingsScreen(Screen):
+    def enter_zybooks(self):
+        websiteName = "zybooks"
 class ClassAddScreen(Screen): pass
 class SignInScreen(Screen): pass
+class WebsiteDataScreen(Screen):
+    def save_input(self):
+        user_name = self.ids.username_field.text
+        user_pass = self.ids.password_field.text
+
 
 # Build and Modify the app
 class UBubbleApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = "Red"
+        self.theme_cls.primary_palette = "Purple"
         self.theme_cls.theme_style = "Dark"
         return
 
 # Run
 if __name__ == "__main__":
     UBubbleApp().run()
+
