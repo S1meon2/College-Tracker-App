4088a378f5ee6c79391381b59372c643d37dcec6
tommorowtime <simeonherrod2@gmail.com>
2026-04-04 21:24:20 -0500

    Integrated SQLite so that the user's connected login info will be saved locally to their device, so that their data stays protected while also allowing their info to not get lost each time the app is started. The user (assuming they have connected their website logins) can press "Update" in settings and the program will scrape all of their assignmets.

---
+++ ububbleDB.py
@@ -0,0 +1,55 @@
+import sqlite3
+from Assignment_WEB import scrape_cengage, scrape_zybooks, scrape_blackboard
+
+connection = sqlite3.connect('website_logins.db')
+
+cursor = connection.cursor()
+
+command1 = """CREATE TABLE IF NOT EXISTS
+
+login(name TEXT PRIMARY KEY, username TEXT, password TEXT)
+
+"""
+
+
+
+#cursor.execute(command1)
+
+def send_info(name, userName, userPass):
+
+    data = [
+        (name, userName, userPass),
+    ]
+
+    # Insert multiple rows efficiently
+    cursor.executemany("INSERT OR IGNORE INTO login (name, username, password) VALUES (?, ?, ?)", data)
+    connection.commit()
+
+    cursor.execute("SELECT * FROM login")
+
+    results = cursor.fetchall()
+
+    #cursor.execute("DELETE * FROM login")
+
+    print(results)
+
+def recieve_info():
+    connection = sqlite3.connect('website_logins.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM login")
+    logins = cursor.fetchall()
+
+    for login in logins:
+
+        if login[0] == "cengage":
+            scrape_cengage(login[0], login[1], login[2])
+
+        if login[0] == "zybooks":
+            scrape_zybooks(login[0], login[1], login[2])
+
+        if login[0] == "blackboard":
+            scrape_blackboard(login[0], login[1], login[2])
+
+
+
+
